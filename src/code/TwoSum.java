71dad6d7ef4fd1610a9ff248f456b40e9c54153b
wangh24 <hao.wang2@emc.com>
2017-10-11 11:55:41 +1030

    new method

--- src/code/TwoSum.java
+++ src/code/TwoSum.java
@@ -1,10 +1,13 @@
 package code;
 
+import java.util.*;
+import java.util.Map.Entry;
+
 public class TwoSum {
 	public static void main(String[] args) {
 		int sum[] = {1, 2 ,4};
 		int target = 5;
-		int result[] = new TwoSum().twoSum(sum, target);
+		int result[] = new TwoSum().twoSum2(sum, target);
 		System.out.print(result[0]);
 		System.out.print(result[1]);
 	}
@@ -25,5 +28,34 @@ public class TwoSum {
         	 }
         }
         return null;
+    }
+	public int[] twoSum2(int[] nums, int target) {
+		int result[] = new int[2];
+		Map<Integer, ArrayList<Integer>> mapTmp =  new HashMap<>();
+		for(int i=0; i<nums.length;i++) {
+			ArrayList<Integer> list = mapTmp.get(nums[i]);
+			if(list == null) {
+				list = new ArrayList<Integer>();
+			}
+			list.add(i);
+			mapTmp.put(nums[i], list);
+		}
+		for(Entry<Integer, ArrayList<Integer>> entry: mapTmp.entrySet()) {
+			int a1 = entry.getKey();
+			ArrayList<Integer> results = mapTmp.get(target-a1);
+			if(results == null) {
+				continue;
+			}
+			if(a1 == target - a1 && results.size()>1) {
+				result[0] = results.get(0);
+				result[1] = results.get(1);
+				break;
+			} else if(results.size() == 1) {
+				result[0] = entry.getValue().get(0);
+				result[1] = results.get(0);
+				break;
+			}
+		}
+		return result;
     }
 }
